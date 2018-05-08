4bd80df157d53005a5a1bca341889c8ffea8951c
James Baster <james.baster@opendataservices.coop>
2018-05-08 14:04:30 +0100

    Split BS4 work into different class so we can work on several different methods at once

--- standardsearch/etl/process.py
+++ standardsearch/etl/process.py
@@ -1,31 +1,30 @@
-import requests
-from bs4 import BeautifulSoup
+
 import standardsearch.elasticsearchfactory
+import standardsearch.etl.processbs4
+
+"""
+Main Process class, that calls specific process classes to do actual work.
+
+The reason for this is that it's not clear at this stage which method will field the best results (bs4, lxml, etc).
+It may even turn out that different methods will be better for different pages.
+In this way, we can work on several alternative methods at once.
+"""
 
 
 class Process:
     elasticsearchfactory = None
     sources = []
+    defaultprocess = None
 
     def __init__(self):
         self.elasticsearchfactory = standardsearch.elasticsearchfactory.ElasticSearchFactory()
+        self.defaultprocess = standardsearch.etl.processbs4.ProcessBS4(self.elasticsearchfactory)
 
     def add_source(self, source):
         self.sources.append(source)
 
     def go(self):
         for source in self.sources:
-            r = requests.get(source.url)
-            if r.status_code == 200:
-                soup = BeautifulSoup(r.text, 'html.parser')
-
-                body = {
-                    'url': source.url,
-                    'text': soup.get_text(),
-                    'title': soup.title.string,
-                }
-                self.elasticsearchfactory.get().index(index=self.elasticsearchfactory.index,
-                                                      doc_type=self.elasticsearchfactory.doctype,
-                                                      id=source.url,
-                                                      body=body
-                                                      )
+            # For now, everything is processed with the default process.
+            # In the future, maybe different sources could specify the method to use?
+            self.defaultprocess.process(source)
