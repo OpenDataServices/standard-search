41e520691f79b5357c69e77938d2c8ce7736e128
Automated Reformatting <code+reformatting@opendataservices.coop>
2020-02-17 11:38:14 +0000

    Add Isort And Black
    As part of this created the new file requirements_dev.txt. Previously needed things (like flake8) were installed in .travis.yml but we want it to be easy for devs to get needed tools locally as well.

--- standardsearch/etl/ocds.py
+++ standardsearch/etl/ocds.py
@@ -1,37 +1,35 @@
-import tempfile
 import os.path
+import tempfile
+
 import standardsearch.etl.extract
-from standardsearch.etl.sources import Source
-from standardsearch.etl.load import load
-
 from standardsearch.etl.extract_sphinx import ExtractSphinx
+from standardsearch.etl.load import load
+from standardsearch.etl.sources import Source
+
+LANG_MAP = {"en": "english", "fr": "french", "es": "spanish", "it": "italian"}
 
 
-LANG_MAP = {'en': 'english',
-            'fr': 'french',
-            'es': 'spanish',
-            'it': 'italian'}
-
-
-def run_scrape(version='latest', langs=('en', 'es', 'fr'), url=None, new_url=None):
+def run_scrape(version="latest", langs=("en", "es", "fr"), url=None, new_url=None):
 
     if not url:
-        url = 'https://standard.open-contracting.org/{}/'.format(version)
+        url = "https://standard.open-contracting.org/{}/".format(version)
 
     for lang in langs:
-        lang_url = url.rstrip('/') + '/' + lang + '/'
+        lang_url = url.rstrip("/") + "/" + lang + "/"
         new_lang_url = None
         if new_url:
-            new_lang_url = new_url.rstrip('/') + '/' + lang + '/'
+            new_lang_url = new_url.rstrip("/") + "/" + lang + "/"
 
         with tempfile.TemporaryDirectory() as tmpdirname:
-            extract_file = os.path.join(tmpdirname, 'extract.json')
+            extract_file = os.path.join(tmpdirname, "extract.json")
             extract = standardsearch.etl.extract.Extract(extract_file)
-            extract.add_source(Source(url=lang_url, new_url=new_lang_url, extractor=ExtractSphinx))
+            extract.add_source(
+                Source(url=lang_url, new_url=new_lang_url, extractor=ExtractSphinx)
+            )
             extract.go()
             load(
                 base_url=(new_lang_url or lang_url),
-                language=LANG_MAP.get(lang, 'standard'),
+                language=LANG_MAP.get(lang, "standard"),
                 extract_file=extract_file,
-                lang_code=lang
+                lang_code=lang,
             )
--- standardsearch/utils.py
+++ standardsearch/utils.py
@@ -1,5 +1,5 @@
 def get_http_version_of_url(url):
-    if url.startswith('https://'):
-        return 'http' + url[5:]
+    if url.startswith("https://"):
+        return "http" + url[5:]
     else:
         return url
