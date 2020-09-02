db018cd8eda064d1ddd12f1fa97a339ca062d5bc
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-09-02 16:56:24 -0400

    Consolidate some files

--- standardsearch/etl/ocds.py
+++ standardsearch/etl/ocds.py
@@ -1,12 +1,48 @@
-import os.path
+import json
+import os
 import tempfile
 
-import standardsearch.etl.extract
 from standardsearch.etl.extract_sphinx import ExtractSphinx
 from standardsearch.etl.load import load
-from standardsearch.etl.sources import Source
 
 LANG_MAP = {"en": "english", "fr": "french", "es": "spanish", "it": "italian"}
+this_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+class Extract:
+    """
+    Main Process class, that calls specific process classes to do actual work.
+
+    The reason for this is that it's not clear at this stage which method will field the best results (bs4, lxml, etc).
+    It may even turn out that different methods will be better for different pages. In this way, we can work on several
+    alternative methods at once.
+    """
+    def __init__(self, extract_file=None):
+        self.sources = []
+        if not extract_file:
+            extract_file = os.path.join(this_dir, "../../extracted_data.json")
+        self.extract_file = extract_file
+
+    def add_source(self, source):
+        self.sources.append(source)
+
+    def go(self):
+        output = []
+        for source in self.sources:
+            output.extend(source.extract())
+
+        with open(self.extract_file, "w+") as f:
+            json.dump(output, f, indent=2, ensure_ascii=False)
+
+
+class Source:
+    def __init__(self, url=None, new_url=None, extractor=None):
+        self.url = url
+        self.new_url = new_url
+        self.extractor = extractor()
+
+    def extract(self):
+        return self.extractor.process(self)
 
 
 def run_scrape(version="latest", langs=("en", "es", "fr"), url=None, new_url=None):
@@ -22,7 +58,7 @@ def run_scrape(version="latest", langs=("en", "es", "fr"), url=None, new_url=Non
 
         with tempfile.TemporaryDirectory() as tmpdirname:
             extract_file = os.path.join(tmpdirname, "extract.json")
-            extract = standardsearch.etl.extract.Extract(extract_file)
+            extract = Extract(extract_file)
             extract.add_source(
                 Source(url=lang_url, new_url=new_lang_url, extractor=ExtractSphinx)
             )
--- standardsearch/etl/sources.py
+++
@@ -1,8 +0,0 @@
-class Source:
-    def __init__(self, url=None, new_url=None, extractor=None):
-        self.url = url
-        self.new_url = new_url
-        self.extractor = extractor()
-
-    def extract(self):
-        return self.extractor.process(self)
